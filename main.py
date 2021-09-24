15e8fde69294929933b2a333751130424585379f
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2021-09-24 14:28:57 -0500

    fixed some typos

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_soup(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     return soup
 
 
-url = "https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/000032019321000065/aapl-20210626.htm#ibbf5b20ab76c4c71bb743a1e5da969e3_13"
+url = """
+    https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/0000320193210000
+    65/aapl-20210626.htm#ibbf5b20ab76c4c71bb743a1e5da969e3_13
+"""
 soup = get_soup(url=url)
 print(soup)
