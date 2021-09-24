a3954e8060632e39588bc8ce4b1a332badfc89ce
Joesph Chrysanthopoulos <joeyc@Joesphs-Mac-mini.local>
2021-09-24 14:47:31 -0500

    fixed some typos

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+# required imports
 from bs4 import BeautifulSoup
 import requests
 
 
+# grab all html
 def get_soup(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     return soup
 
 
+# Apple 10-q url
 url = """
     https://www.sec.gov/ix?doc=/Archives/edgar/data/320193/0000320193210000
     65/aapl-20210626.htm#ibbf5b20ab76c4c71bb743a1e5da969e3_13
 """
+
 soup = get_soup(url=url)
 print(soup)
